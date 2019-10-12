00996023ae791bcd74caa6287ab4b1a7a1169653
Massu <imomushi.pipi@gmail.com>
2019-10-12 18:25:55 +0900

    [add] 新規作成

--- post.py
+++ post.py
@@ -8,19 +8,22 @@ def post():
 
     # POSTするデータ
     data = {
-        "token": "access token",
-        "type": "処理の種類 (update)",
-        "ratio": "ホコリの比率[%]",
-        "concent": "濃度[pcs/0.01cf]",
-        "temp": "気温[℃]",
-        "humid": "湿度[%]",
-        "press": "気圧[hPa]",
-        "light": "明るさ",
-        "rain": "雨(センサ, bool)",
-        "current": "現在雨が降っているか(気象予報API, bool)",
-        "forecast": "この後雨が降るか(bool)"
+        "token": "",            # access token
+        "type": "update",       # 処理の種類 (update)
+        "ratio": "",            # ホコリの比率[%]
+        "concent": "",          # ホコリの濃度[pcs/0.01cf]
+        "temp": "",             # 気温[℃]
+        "humid": "",            # 湿度[%]
+        "press": "",            # 気圧[hPa]
+        "light": "",            # 明るさ
+        "wind": "",             # 風が強いか(bool)
+        "rain": "",             # 雨(センサ, bool)
+        "current": "",          # 現在雨が降っているか(気象予報API, bool)
+        "forecast": ""          # この後雨が降るか(bool)
     }
 
     # POST
-    response = requests.post(
-        "POSTするURL", headers=headers, data=json.dumps(data))
+    response = requests.post("POSTするURL", headers=headers, data=json.dumps(data))
+
+if __name__ == "__main__":
+    post()
