f77dac88ecd1b9c483883ec3ad46f606234f2bf3
Massu <imomushi.pipi@gmail.com>
2019-10-13 02:43:13 +0900

    [update] リネーム・更新

--- weather.py
+++ weather.py
@@ -5,9 +5,9 @@ class WeatherAPI():
     """
     気象情報API処理
 
-    Attributes
+    Parameters
     -----------
-    configjson : str
+    jsonpath : str
         appid、位置情報が記述されたjsonのファイルパス
         jsonには以下のよう記述
         {
@@ -17,24 +17,39 @@ class WeatherAPI():
         }
 
     """
-    def __init__(self, configjson):
+    def __init__(self, jsonpath):
 
-        with open(configjson, 'r') as cf:
+        with open(jsonpath, 'r') as cf:
             config = json.load(cf)
         self.appid = config["appid"]
         self.longitude = config["longitude"]
         self.latitude = config["latitude"]
 
     def get(self):
+        """
+        天気・降水量取得
+        """
+
+        # YOLPリクエストパラメーター
         payload = {
-            "appid" : self.appid,
-            "output" : 'json',
-            "coordinates" : self.longitude + ',' + self.latitude
+            "appid" : self.appid,   # Client ID
+            "output" : 'json',      # 出力方式
+            "coordinates" : self.longitude + ',' + self.latitude    # 位置情報(経度, 緯度)
         }
+
+        # リクエスト
         r = requests.get("https://map.yahooapis.jp/weather/V1/place", params=payload)
+
+        # レスポンスをjsonに変換
         data = r.json()
-        print(data)
+
+        # 降水量を抽出
+        weather_list = data['Feature'][0]['Property']['WeatherList']['Weather']
+        precipitation = weather_list[0]['Rainfall']
+
+        return precipitation
+
 
 if __name__ == '__main__':
     weatherapi = WeatherAPI("config.json")
-    weatherapi.get()
+    print(weatherapi.get())
